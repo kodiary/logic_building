36d15bfc05954ac7aa71cbda338b72cef02b6951
Anwar Ali <abuyusuf2045@gmail.com>
2020-04-01 16:28:10 +0545

    Prime part I and part II

---
+++ prime.php
@@ -0,0 +1,24 @@
+<?php
+// Print out all numbers from 1 - 100 that are prime ... 2,3,5,7,11,13,17,19,23......
+// 6 => 2,3
+// Checking the number by all other numbers that are smaller than the number
+// 6 => 5,4,3,2
+// 4 => 3,2
+// 7 => 6,5,4,3,2
+$counter = 0;
+for($i=2;$i<=100;$i++){
+  $flag = 0;
+  for($j=$i;$j>=2;$j--){
+    $counter++;
+    if($j==$i){
+      continue;
+    }
+    if($i%$j==0){
+      $flag = 1;
+    }
+  }
+  if($flag==0){
+    echo $i."<br />";
+  }
+}
+echo "<p>Total loop: ".$counter."</p>";
